2baa40ff7ac48a069442657c3862559f6777cb51
Marko Zecevic <marczecevic@gmail.com>
2021-07-27 12:34:08 +0200

    Add focus on load to input with useRef

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,4 +1,4 @@
-import React ,{useState,useEffect} from 'react'
+import React ,{useState,useEffect,useRef} from 'react'
 import DisplayWeather from './DisplayWeather.'
 
 
@@ -6,10 +6,10 @@ function WeatherContainer() {
 
     const[city,setCity] = useState('')
     const[data,setData] = useState()
-
+    const inputRef = useRef()
     useEffect(()=>{
-        
-    })
+        inputRef.current.focus()
+    },[])
 
     const onClickSearch = ()=>{
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e933e63cbe1c79cbe7e1844bf80fe3f0`)
@@ -23,7 +23,7 @@ function WeatherContainer() {
 
     return (
         <div className='container'>
-            <input type="text" className="form-control" placeholder='Enter city name' value={city} onChange={(e)=>setCity(e.target.value)}/>
+            <input type="text" className="form-control" placeholder='Enter city name' value={city} onChange={(e)=>setCity(e.target.value)} ref={inputRef} />
             <button onClick={onClickSearch} className='btn btn-primary form-control'>Search</button>
             <DisplayWeather data={data} city={city}/>
         </div>
